5bcf5f7fb3185e39066c505012a9cea8d7250c0d
tommy martinez <thomasjohnmartinez@gmail.com>
2022-08-19 00:52:20 -0400

    working audio engine

--- src/App.js
+++ src/App.js
@@ -8,12 +8,15 @@ function App() {
       <header className="App-header">
         {/*<img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Spectrum-Writer is a Web3 music composition tool.
+          Spectrum-Writer is a Collaborative Web3 Music Composition tool.
         </p>
-
+        <p>
+          Collect Spectral NFTs for access to the
+        </p>
         <p>
           Connect your Metamask Wallet to get started.
         </p>
+
       </header>
 
     </div>
