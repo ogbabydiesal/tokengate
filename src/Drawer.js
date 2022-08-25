60ec70740eec7ea29f44971c2f94eb1fde992d46
tommy martinez <thomasjohnmartinez@gmail.com>
2022-08-25 15:11:49 -0400

    pushAug

--- src/Drawer.js
+++ src/Drawer.js
@@ -1,47 +1,95 @@
-import React from "react";
+import React, {useState} from "react";
+import Player from './Player';
+
 
 export default function App() {
+  const [specFrame, setSpecFrame] = useState(0);
   React.useEffect(() => {
-    let spectrum = new Array(12800).fill(0);
-    let fftSize = 256;
-    let column = 0;
-    let row = 0;
 
+    //setup the spectrum and overlay drawing contexts
+    let c = document.getElementById("myCanvas");
+    let ctx = c.getContext("2d");
+    let d = document.getElementById("myCanvas2");
+    let ctx2 = d.getContext("2d");
+    //create a spectrum array
+    let fftSize = 128;
+    let spectrum = new Array(fftSize * 50).fill(0); //6400
+    //generate random values for our spectrum
     for (let i = 0; i < spectrum.length; i++) {
-      if (Math.random() < .8) {
+      if (Math.random() < .01) {
         spectrum[i] = Math.random().toFixed(3);
       }
     }
-
-    let c = document.getElementById("myCanvas");
-    let ctx = c.getContext("2d");
+    //spectrum drawing
+    let column = 0;
+    let row = 0;
+    let outputSizeX = 10;
+    let outputSizeY = 3; //multiplier for spectrum visualization
     for (let x = 0; x < spectrum.length; x++) {
       if (x % fftSize == 0) {
         column++;
       }
       row = x % fftSize;
       ctx.fillStyle = 'rgba(' + (spectrum[x]* 255)+','+ (spectrum[x]* 255)+','+(spectrum[x]* 255) + ')';
-      //console.log(column);
-      //ctx.fillStyle = 'rgb(100, 100, 100)';
-      ctx.fillRect(column - 1,row,1, 1);
-
+      ctx.fillRect((column * outputSizeX)-outputSizeX,row * outputSizeY,outputSizeX, outputSizeX);
     }
-    /*
-    ctx.moveTo(0, 0);
-    ctx.lineTo(50, 256);
-    ctx.stroke();
-    */
+    //
+    let xPos = 0;
+
+    let start, previousTimeStamp;
+    let done = false
+
+    function step(timestamp, specFrame) {
+      if (start === undefined) {
+        start = timestamp;
+        }
+      const elapsed = timestamp - start;
+      if (previousTimeStamp !== timestamp) {
+        // Math.min() is used here to make sure the element stops at exactly 200px
+        const count = Math.min(0.1 * elapsed, 200);
+        ctx2.fillStyle = 'royalblue';
+        ctx2.clearRect(0, 0, 500, 384)
+        ctx2.fillRect(xPos, 0, 13, 384) //play head
+        let specFrame = spectrum.slice(xPos * 128, 128);
+        setSpecFrame(specFrame);
+        //console.log(spectrum);
+        xPos++;
+        if (xPos == 500) xPos = 0;
+        if (count === 200) done = true; //probably lose this for a button
+      }
+      window.requestAnimationFrame(step);
+      if (elapsed < 2000) { // Stop the animation after condition
+        previousTimeStamp = timestamp;
+        if (!done) {
+          //window.requestAnimationFrame(step);
+        }
+      }
+    }
+    window.requestAnimationFrame(step);
+    //return?
   }, []);
 
   return (
     <div>
-      <canvas className="canvasConnect"
+      <canvas className="canvasSpectrum"
         id="myCanvas"
-        width="50"
-        height="256"
+        width="500"
+        height="384"
       >
         Your browser does not support the HTML canvas tag.
       </canvas>
+      <canvas className="canvasOverlay"
+        id="myCanvas2"
+        width="500"
+        height="384"
+      >
+      </canvas>
+      <Player specFrame={specFrame}/>
     </div>
+
+
+
+
+
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,18 +4,15 @@ import './index.css';
 import './App.css';
 import App from './App';
 import Metamask from './Metamask';
-import Player from './Player';
 import Drawer from './Drawer';
-
+// ./Player is a child of Drawer
 import reportWebVitals from './reportWebVitals';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <Metamask />
-    <Player />
     <Drawer />
   </React.StrictMode>
 );
