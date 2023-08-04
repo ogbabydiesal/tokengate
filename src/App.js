44f75e8aaab97a9f8f621b46f184dc06cb1a1912
tommy martinez <thomasjohnmartinez@gmail.com>
2023-08-04 17:41:55 -0400

    added github repo url

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
         <h1>
           Token Gate
         </h1>
+        <p>code at <a href="https://github.com/tmartinez88/tokengate" target="_blank" rel="noreferrer">https://github.com/tmartinez88/tokengate</a></p>
     </div>
   );
 }
